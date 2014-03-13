da448afb98d01c6a4f86fbb909b3c8209f94fa08
farseer90718 <farseer90718@gmail.com>
2014-03-13 23:16:29 +0800

    color

--- mwin.cpp
+++ mwin.cpp
@@ -18,18 +18,8 @@ mwin::mwin() {
     noecho();
     start_color();
 
-    init_pair(0, COLOR_BLACK, COLOR_WHITE);
-    init_pair(1, COLOR_BLACK, COLOR_RED);
-    init_pair(2, COLOR_BLACK, COLOR_YELLOW);
-    init_pair(3, COLOR_BLACK, COLOR_BLUE);
-    init_pair(4, COLOR_BLACK, COLOR_GREEN);
-    init_pair(5, COLOR_BLACK, COLOR_CYAN);
-    init_pair(6, COLOR_BLACK, COLOR_MAGENTA);
-    init_pair(7, COLOR_BLACK, COLOR_WHITE);
-    init_pair(8, COLOR_BLACK, COLOR_GREEN);
-    init_pair(9, COLOR_BLACK, COLOR_CYAN);
-    init_pair(10, COLOR_BLACK, COLOR_MAGENTA);
-    init_pair(11, COLOR_BLACK, COLOR_WHITE);
+    for (int i = 0; i < 15; i++)
+        init_pair(i, COLOR_BLACK, 65+3*i);
 
     curs_set(0); // remove curser
     refresh();
